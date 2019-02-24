1772273122aa6c1f1f5f70d5dfe5c45001fc032b
Ahsan Javed <iahsanjaved@yahoo.com>
2019-02-24 23:41:28 +0500

    'Hello!'

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+
+	async create({ request, response, auth }) {
+		const user = await User.create(request.only(['username', 'password', 'email']));
+		await auth.login(user);
+		return response.redirect('/');
+	}
+
+	async login({ request, response, auth, session }) {
+		const { email, password } = request.all();
+
+		try {
+			await auth.attempt(email, password)
+			return response.redirect('/');
+		} catch (error) {
+			session.flash({ loginError: 'These credentials do not work.' });
+			return response.redirect('back');
+		}
+
+	}
+}
+
+module.exports = UserController
